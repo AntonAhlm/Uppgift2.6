e52225b31c912ce6aa59f22ce8564f982bed77f1
Anton <>
2023-01-27 09:37:03 +0100

    Add project files.

---
+++ Uppgift2.6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening med 3 ord.");
+            string Mening = Console.ReadLine();
+
+            int PunktIndexx = Mening.IndexOf(" ");
+            int PunktIndex = Mening.IndexOf(" ",PunktIndexx);
+
+            string ord1 = Mening[..PunktIndex];
+
+            string ord2 = Mening[(PunktIndex + 1^1)..];
+
+            string ord3 = Mening[(PunktIndex + 3)..];
+
+            //Console.WriteLine(ord3 + ord2 + ord1 + ".");
+            Console.WriteLine(ord1);
+            Console.WriteLine(ord2);
+            Console.WriteLine(ord3);
+
+        }
+    }
+}
